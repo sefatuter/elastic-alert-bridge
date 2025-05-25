56663e02f9cdd10e901dee57e8f8702e426609a1
sefatuter <sefa2233_18@hotmail.com>
2025-05-26 00:06:33 +0300

    Clean and start again

--- api/routes/web.php
+++ api/routes/web.php
@@ -7,13 +7,3 @@ use App\Http\Controllers\ElastAlertRulesController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/logs', [LogController::class, 'index']);
-Route::get('/logs/indices', [LogController::class, 'showIndices'])->name('logs.indices');
-Route::get('/logs/search', [LogController::class, 'searchIndex'])->name('logs.search');
-
-Route::get('/elastalert-rules', [ElastAlertRulesController::class, 'index'])->name('elastalert_rules.index');
-Route::get('/elastalert-rules/create', [ElastAlertRulesController::class, 'create'])->name('elastalert_rules.create');
-Route::post('/elastalert-rules', [ElastAlertRulesController::class, 'store'])->name('elastalert_rules.store');
-Route::get('/elastalert-rules/{filename}/edit', [ElastAlertRulesController::class, 'edit'])->name('elastalert_rules.edit');
-Route::put('/elastalert-rules/{filename}', [ElastAlertRulesController::class, 'update'])->name('elastalert_rules.update');
